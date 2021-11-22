acc3eb876b12b12836199002b83f6924318109de
Afshin Rahimi <afshinrahimi@gmail.com>
2021-11-22 12:14:39 +1000

    added clinicaltrials ids to the returned list

--- pubmed_parser/medline_parser.py
+++ pubmed_parser/medline_parser.py
@@ -480,10 +480,8 @@ def parse_article_ncts(article):
     data_banks = article.findall("DataBankList/DataBank")
     for data_bank in data_banks:
         data_bank_name = data_bank.find('DataBankName')
-        #print(data_bank_name.text)
         if data_bank_name.text.strip() == "ClinicalTrials.gov":
-            print(data_bank_name.text)
-            NCTs = [node.text for node in data_bank.findall('AccessionNumberList/AccessionNumber')]
+            NCTs = [node.text.strip() for node in data_bank.findall('AccessionNumberList/AccessionNumber')]
             return NCTs
     return []
                                        
