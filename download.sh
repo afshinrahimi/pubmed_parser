aeb3a30412d18bbbb4b20da3fcff6e95e601c7eb
Afshin Rahimi <afshinrahimi@gmail.com>
2021-11-22 17:54:34 +1000

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,2 +1,2 @@
 nohup wget -r -l1 -H -t1 -nd -N -np -A.gz -erobots=off   https://ftp.ncbi.nlm.nih.gov/pubmed/baseline &
-nohup wget -r -l1 -H -t1 -nd -N -np -A.gz -erobots=off   https://ftp.ncbi.nlm.nih.gov/pubmed/updatefiles/ &
+nohup wget -r -l1 -H -t1 -nd -N -np -A.gz -erobots=off -P daily   https://ftp.ncbi.nlm.nih.gov/pubmed/updatefiles/  &
